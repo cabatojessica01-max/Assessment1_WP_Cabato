daf44e00977c580b6be86a6bff214d7f23f7e463
cabatojessica01-max <cabatojessica01@gmail.com>
2026-04-28 21:49:05 +0300

    Delete 05_Embedded Content directory

--- 05_Embedded Content/bonus_05 (soundboard)/index.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll(".sound");
-
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const soundFile = button.getAttribute("data-sound");
-    const audio = new Audio("Audio/" + soundFile);
-    audio.play();
-  });
-});
